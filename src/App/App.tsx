d301971da8aebcaa0570a020247d75f27a96b4aa
gaurav dalasania <dabc0078@gmail.com>
2022-04-13 22:40:23 +0530

    tic tac toe initial setup with typescript

---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+import Playground from "../components/Playground/Playground";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<Playground />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Playground/Playground.tsx
@@ -0,0 +1,11 @@
+import "./Playground.css";
+
+const Playground = () => {
+	return (
+		<div>
+			<h1>this is playground page</h1>
+		</div>
+	);
+};
+
+export default Playground;
